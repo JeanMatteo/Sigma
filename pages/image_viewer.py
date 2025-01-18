8539c4d83b7ecf791ae75c4949441b4910512f3d
JeanMatteo <jean.citro.032162047@sdh.sch.id>
2025-01-18 12:02:56 +0700

    Update image_viewer.py

--- pages/image_viewer.py
+++ pages/image_viewer.py
@@ -3,13 +3,13 @@ import os
 
 col1,col2,col3 = st.columns(3)
 with col1:
-    st.image(r'Images/Earth.png')
+    st.image(r'./images/Earth.png')
 with col2:
-    st.image(r'Images/microsoft_teams.png')
+    st.image(r'./images/microsoft_teams.png')
 with col3:
-    st.image(r'Images/thonny-logo.png')
+    st.image(r'./images/thonny-logo.png')
     
-folder = os.listdir('Images')
+folder = os.listdir('images')
 st.write(folder)
 
 col1, col2 = st.columns(2)
@@ -17,6 +17,6 @@ with col1:
     selection = st.selectbox("Choose the picture",options = folder)
 
 with col2:
-    st.image(r'Images/'+ selection)
+    st.image(r'./images/'+ selection)
 
 
