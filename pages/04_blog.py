25fe6185aacf8a27df8378f784f083c4de245294
JeanMatteo <jean.citro.032162047@sdh.sch.id>
2025-02-22 11:31:21 +0700

    Update 04_blog.py

--- pages/04_blog.py
+++ pages/04_blog.py
@@ -4,7 +4,8 @@ import os
 files = os.listdir('./pages/blog_files') 
 
 for each_file in files:
-  with open('./pages/blog_files/' + each_file,'r') as f:
-    content = f.read()
-  f.close()
-  st.markdown(content)
+  with st.container(border=True):
+    with open('./pages/blog_files/' + each_file,'r') as f:
+      content = f.read()
+    f.close()
+    st.markdown(content)
