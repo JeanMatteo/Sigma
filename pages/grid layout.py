299f419142f011542284e4ce4cfa1cb78f225fd4
JeanMatteo <jean.citro.032162047@sdh.sch.id>
2025-02-22 12:01:08 +0700

    Create grid layout.py

---
+++ pages/grid layout.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+ncolumn = 4
+
+column = st.columns(ncolumn)
+
+for i in range(ncolumn):     
+    if i%ncolumn == 0:
+        col = columns[0]
+    if i%ncolumn == 1:
+        col = columns[1]
+    if i%ncolumn == 2:
+      col = columns[2]
+    if i%ncolumn == 3:
+      col = columns[3]
+
+    with col:
+      with st.container(border=True):
+          st.write("Hello")
