b07f2ed1ba8af23e972ced38fe5c3c99fbc9ddcf
JeanMatteo <jean.citro.032162047@sdh.sch.id>
2025-02-22 12:05:49 +0700

    Update grid layout.py

--- pages/grid layout.py
+++ pages/grid layout.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-ncolumn = 4
+ncolumn = 6
 
 columns = st.columns(ncolumn)
 
@@ -13,6 +13,10 @@ for i in range(ncolumn):
         col = columns[2]
     if i%ncolumn == 3:
         col = columns[3]
+    if i%ncolumn == 4:
+        col = columns[4]
+    if i%ncolumn == 5:
+        col = columns[5] 
 
     with col:
       with st.container(border=True):
